71968bdccc6e111adc807684bf90fd4ae8ae971f
Patrick Code <wendeel.admpar@gmail.com>
2021-08-30 14:03:08 -0400

    add itens

--- Aula03/gamestoreapp/src/index.js
+++ Aula03/gamestoreapp/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Video, Mapa, Botao, Games } from "./App";
+
+export default function Home() {
+  return <h1>Meu primeiro React</h1>;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
+    <Games />
+    <Botao nome="acumular" />
+    <Video />
+    <Mapa />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
