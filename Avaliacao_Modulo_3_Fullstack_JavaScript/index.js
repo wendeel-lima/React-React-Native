83537b933638958bf84c6439c36fd86d3025ca56
Patrick Code <wendeel.admpar@gmail.com>
2021-09-09 17:23:09 -0400

    implementação de codigo

--- Avaliacao_Modulo_3_Fullstack_JavaScript/index.js
+++ Avaliacao_Modulo_3_Fullstack_JavaScript/index.js
@@ -1,16 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const Conn = require("./models/conn/tarefa.conn");
 
 const app = express();
 
 app.use(express.json());
-const porta = 3000;
 
-Conn("localhost", "27017", "tarefas");
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
+Conn(db_url, db_user, db_pass, db_data);
+
+const porta = 3000;
 
 const todo = require("./routers/tarefa.routes");
 app.use("/tarefa", todo);
 
-app.listen(porta, () => {
+app.listen(process.env.PORT || porta, () => {
   console.info(`app rodando em : http://localhost:${porta}/`);
 });
--- Avaliacao_Modulo_3_Fullstack_JavaScript/models/conn/tarefa.conn.js
+++ Avaliacao_Modulo_3_Fullstack_JavaScript/models/conn/tarefa.conn.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
-const Conn = (url, porta, banco) => {
+const conect = (db_url, db_user, db_pass, db_data) => {
+  db_url = process.env.DB_URL;
+  db_user = process.env.DB_USER;
+  db_pass = process.env.DB_PASS;
+  db_data = process.env.DB_DATA;
+};
+const Conn = (url, user, pass, banco) => {
   mongoose
-    .connect(`mongodb://${url}:${porta}/${banco}`, {
+    .connect(`${url}/${banco}`, {
+      user: user,
+      pass: pass,
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
